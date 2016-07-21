05113577fc773d197bbbbc6a77c6f98fe132bdb9
nohsunghyun <nsh1090@brickand.com>
2016-07-21 20:33:53 +0900

    make project

---
+++ web-client/src/test/java/org/ahea/blindinterview/client/web/WebClientApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ahea.blindinterview.client.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = WebClientApplication.class)
+public class WebClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
