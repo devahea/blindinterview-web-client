4bfc5ba5247e6fa16babfc942a727e06e9015a22
jongin <a@DESKTOP-EUI1G9F>
2016-08-03 21:35:26 +0900

    jsp 파일 2개 추가및 컨트롤러 패스 추가

--- web-client/src/main/java/org/ahea/blindinterview/client/web/home/HomeController.java
+++ web-client/src/main/java/org/ahea/blindinterview/client/web/home/HomeController.java
@@ -10,5 +10,17 @@ public class HomeController {
 	public String hello() {
 		return "hello"; 
 	}
+	
+	@RequestMapping("/hello2")
+	public String hello2() {
+		return "hello2"; 
+	}
+
+	
+	@RequestMapping("/hello3")
+	public String hello3() {
+		return "hello3"; 
+	}
+
 
 }
